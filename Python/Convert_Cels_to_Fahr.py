531816c74abec5a9d2ce3b441ebb7c31a5c09484
Hamna Qaseem <72624285+Hamna12@users.noreply.github.com>
2021-10-17 19:15:37 +0500

    Create Convert_Cels_to_Fahr.py

---
+++ Python/Convert_Cels_to_Fahr.py
@@ -0,0 +1,15 @@
+#first prompt the user to enter temperature in celsius
+cels=float(input("Enter the temperature in celsius: "))
+
+#calculate fahrenheit by using formula
+fahr=cels*1.8+32
+
+#print temperature in fahrenheit
+print("Temperature in fahrenheit: ", fahr)
+
+Output:
+#Enter value of celsius
+Enter the temperature in celsius: 30
+  
+#calculate fahrenheit
+Temperature in fahrenheit:  86.0
